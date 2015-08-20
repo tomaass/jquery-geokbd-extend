8b84027abc594401e791129337d2db55019c036a
paulslaby <paul.slaby@seznam.cz>
2015-08-20 17:25:58 +0200

    Supress keypress if input is readonly

--- src/jquery.geokbd.js
+++ src/jquery.geokbd.js
@@ -78,6 +78,8 @@ $.fn.geokbd = function(options) {
 	$(document).keypress(function(e) {
 		if (e.target.type === 'password' || e.target.type === 'email') {return;}
 
+		if ($(e.target).prop("readonly")) { return; }
+
 		if ( $(e.target).attr('maxlength') !== undefined ) {
 			var limit = parseInt($(e.target).attr('maxlength'));
 			var currentLength = $(e.target).val().length;
